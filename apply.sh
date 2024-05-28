4b12ef9df03d4188b800b43f3c6555881377546b
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-05-28 22:43:12 +0530

    easy way to manage git credentials

--- apply.sh
+++ apply.sh
@@ -4,3 +4,6 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # commment below line to stop sudo override
 eval "source $SCRIPTPATH/runUserBinariesWithSudo/script"
+
+# comment below line to disable push command
+eval "source $SCRIPTPATH/gitPush/script"
