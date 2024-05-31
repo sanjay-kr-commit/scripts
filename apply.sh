169fcf03d9eebbae122dc242039680dbb3db9990
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-06-01 01:23:51 +0530

    add a unfied way of debugging

--- apply.sh
+++ apply.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/bash 
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+source "$SCRIPTPATH/debug"
+
+if [[ "$1" == "log" ]] ; then
+  constructDebuger
+fi 
 
 # commment below line to stop sudo override
 eval "source $SCRIPTPATH/runUserBinariesWithSudo/script"
@@ -10,3 +16,5 @@ eval "source $SCRIPTPATH/gitPass/script"
 
 # comment below line to disable flatpakAliases
 eval "source $SCRIPTPATH/flatpakAliases/script"
+
+destructDebugger
