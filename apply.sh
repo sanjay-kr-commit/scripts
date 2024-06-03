2b76870b651d7f99672ceeb86be0391fadd46c66
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2024-06-03 23:48:50 +0530

    make script path variable local

--- apply.sh
+++ apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+local SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 source "$SCRIPTPATH/debug"
 
